e499548453b9e0f208530b9acf223c7c964ed82c
Lucas Silvestre <lukas.silvestre@gmail.com>
2019-08-29 10:48:09 +0200

    Fix DelegatesInfo implementation

--- framework/src/modules/chain/dpos/dpos.js
+++ framework/src/modules/chain/dpos/dpos.js
@@ -18,14 +18,7 @@ const { DelegatesList, EVENT_ROUND_FINISHED } = require('./delegates_list');
 const { DelegatesInfo } = require('./delegates_info');
 
 module.exports = class Dpos {
-	constructor({
-		storage,
-		slots,
-		activeDelegates,
-		logger,
-		schema,
-		exceptions = {},
-	}) {
+	constructor({ storage, slots, activeDelegates, logger, exceptions = {} }) {
 		this.finalizedBlockRound = 0;
 		this.slots = slots;
 		this.delegatesList = new DelegatesList({ storage, logger, exceptions });
@@ -33,7 +26,6 @@ module.exports = class Dpos {
 			storage,
 			slots,
 			activeDelegates,
-			schema,
 			logger,
 			delegatesList: this.delegatesList,
 			exceptions,
